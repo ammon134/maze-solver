a460195cfe125433604d50d427197914262b03ae
ammon134 <ammon134@gmail.com>
2024-02-10 13:14:59 +0700

    ft: add maze break_walls_r

--- maze.py
+++ maze.py
@@ -1,9 +1,9 @@
-from typing import List
+from typing import Dict, List
 
 from cell import Cell
 from graphics import Point, Window
 
-import time
+import random
 
 
 class Maze:
@@ -25,8 +25,9 @@ class Maze:
         self.__cell_height = cell_height
         self.__win = win
 
-        self.cells = self.__create_cells()
+        self.__cells = self.__create_cells()
         self.__create_entrance_and_exit()
+        self.__break_walls_r(0, 0)
 
     def __create_cells(self) -> List[List[Cell]]:
         cells = []
@@ -43,24 +44,89 @@ class Maze:
                 )
                 cell = Cell(p1, p2, self.__win)
                 cell.draw("black")
-                self.__win.redraw()
-                time.sleep(0.05)
+                self.__animate()
 
                 row.append(cell)
             cells.append(row)
         return cells
 
     def __animate(self) -> None:
+        if not self.__win:
+            return
+
         self.__win.redraw()
-        time.sleep(0.05)
+        # time.sleep(0.05)
+
+    def get_cells(self) -> List[List[Cell]]:
+        return self.__cells
 
     def __create_entrance_and_exit(self) -> None:
-        entrance = self.cells[0][0]
+        entrance = self.__cells[0][0]
         entrance.has_top = False
         entrance.draw("black")
         self.__animate()
 
-        exit_cell = self.cells[self.__row_count - 1][self.__col_count - 1]
+        exit_cell = self.__cells[self.__row_count - 1][self.__col_count - 1]
         exit_cell.has_bottom = False
         exit_cell.draw("black")
         self.__animate()
+
+    def __break_walls_r(self, i: int, j: int) -> None:
+
+        if not self.__win:
+            print("Window not available, not drawing")
+            return
+
+        current_cell = self.__cells[i][j]
+        current_cell.visited = True
+
+        # directional definitions
+        directions = {
+            "up": {"x": i - 1, "y": j},
+            "down": {"x": i + 1, "y": j},
+            "left": {"x": i, "y": j - 1},
+            "right": {"x": i, "y": j + 1},
+        }
+
+        while True:
+
+            allowed_directions: Dict[str, Dict[str, int]] = {}
+            for dir in directions:
+                x = directions[dir]["x"]
+                y = directions[dir]["y"]
+                if (
+                    0 <= x < self.__row_count
+                    and 0 <= y < self.__col_count
+                    and self.__cells[x][y].visited is False
+                ):
+                    allowed_directions[dir] = directions[dir]
+
+            if len(allowed_directions) == 0:
+                return
+
+            # Choose a random direction
+            # random.seed(1)
+            chosen_direction_name = random.choice(list(allowed_directions.keys()))
+            chosen_direction = allowed_directions[chosen_direction_name]
+
+            # Break wall between these two cells
+            next_cell = self.__cells[chosen_direction["x"]][chosen_direction["y"]]
+
+            if chosen_direction_name == "left":
+                current_cell.has_left = False
+                next_cell.has_right = False
+            if chosen_direction_name == "up":
+                current_cell.has_top = False
+                next_cell.has_bottom = False
+            if chosen_direction_name == "right":
+                current_cell.has_right = False
+                next_cell.has_left = False
+            if chosen_direction_name == "down":
+                current_cell.has_bottom = False
+                next_cell.has_top = False
+
+            current_cell.draw("black")
+            next_cell.draw("black")
+            self.__animate()
+
+            self.__break_walls_r(chosen_direction["x"], chosen_direction["y"])
